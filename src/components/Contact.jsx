d41df54dcb67cd6862ae372007299cc79f79d8e8
andre daniel <andre.emidio09@gmail.com>
2024-02-03 17:51:08 +0000

    contact changes 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,17 +36,13 @@ const Contact = () => {
 
     emailjs
       .send('service_mqyfj1r','template_mr2kzin',
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "andré silva",
           from_email: form.email,
           to_email: "andre.emidio09@gmail.com",
           message: form.message,
-        },'ernEFj-iVufXzHduC',
-
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        },'ernEFj-iVufXzHduC'
       )
       .then(
         () => {
