6c78c6977f15f0af6e90e47926af259e4f12796a
André Silva <153619712+andreatsilva@users.noreply.github.com>
2024-02-06 16:16:44 +0000

    text alignment and other stuff

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Tilt from "react-tilt";
-import Typed from "typed.js"; // Import Typed.js library
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -14,6 +13,7 @@ const ServiceCard = ({ index, title, icon }) => (
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+   
     >
       <div
         options={{
@@ -38,41 +38,33 @@ const ServiceCard = ({ index, title, icon }) => (
 );
 
 const About = () => {
-  useEffect(() => {
-    // Initialize Typed.js instance
-    const typed = new Typed("#typed", {
-      strings: [
-        "I'm a software developer who has gained valuable experience working with languages like C++, Python, and JavaScript. I've also delved into libraries such as OpenGL and Three.js, using them to craft impressive graphics and 3D visuals. In the realm of web design, I rely on Tailwind CSS to create responsive layouts and React.js to develop dynamic user interfaces. My approach to development is fueled by a relentless drive to expand my knowledge base. I thrive on learning and am constantly seeking out new technologies to master. I pride myself on being a fast learner, always eager to explore and delve into the latest tools and techniques. This hunger for knowledge fuels my ability to deliver impactful software solutions that meet the needs of users effectively.",
-      ],
-      typeSpeed: 50, // Typing speed in milliseconds
-      backSpeed: 20, // Backspacing speed in milliseconds
-      loop: false, // Whether to loop the animation
-      showCursor: false, // Hide the typing cursor
-      startDelay: 0,
-    });
+  const [typedText, setTypedText] = useState("");
+  const fullText = "I'm a software developer who has gained valuable experience working with languages like C++, Python, and JavaScript. I'm also have experience in libraries such as OpenGL and Three.js, using them to craft impressive graphics and 3D visuals. In web design, I rely on Tailwind CSS to create responsive layouts and React.js to develop dynamic user interfaces. My approach to development is fueled by a relentless drive to expand my knowledge base. I thrive on learning and am constantly seeking out new technologies to master. I'm fast learner, always eager to explore the latest tools and technologies. This hunger for knowledge fuels my ability to deliver impactful software solutions that meet the needs of users effectively.";
 
-    // Cleanup function to destroy Typed instance
-    return () => {
-      typed.destroy();
-    };
-  }, []); // Run effect only once after the component mounts
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      if (typedText.length < fullText.length) {
+        setTypedText(fullText.substring(0, typedText.length + 1));
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 20);
+
+    return () => clearInterval(typingInterval);
+  }, [typedText, fullText]);
 
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div className="text-center content-center" variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      {/* Text container for Typed.js */}
-      <div id="typed" className="typed-text mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-      
-</div>
+      <div className="mt-4 text-center content-center text-secondary text-[17px]  leading-[30px]">
+        {typedText}
+      </div>
     </>
   );
 };
 
 export default SectionWrapper(About, "about");
-
-
-
